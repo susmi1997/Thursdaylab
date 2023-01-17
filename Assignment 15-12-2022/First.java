eda2527eea6cb0262ff0c3a237506d4adc1c1cd7
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-17 16:34:00 +0530

    Create First.java

---
+++ Assignment 15-12-2022/First.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class First {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			System.out.println("hello");
+			
+		}
+		
+		System.out.println();
+		
+		for(int i=5; i>0;i--) {
+			System.out.println("world");
+		}
+
+	}
+
+}
